fc8756f662302c715e23a3b00f630ce277e1f0c7
Sumit Prasad <sumitprasad585@gmail.com>
2024-09-02 16:01:36 +0100

    error handling for dev and production

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger/swaggerSpec');
 const jobRouter = require('./routes/jobRoutes');
 const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/AppError');
 
 const app = express();
 
@@ -21,5 +23,13 @@ app.use(express.json());
 /** Mount the routers */
 app.use('/api/v1/jobs', jobRouter);
 app.use('/api/v1/users', userRouter);
+
+app.use('*', (req, res, next) => {
+  const message = `The resource ${req.method} ${req.originalUrl} is not found on the server ${req.get('host')}`;
+  const appError = new AppError(404, message);
+  next(appError);
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ controllers/errorController.js
@@ -0,0 +1,57 @@
+const AppError = require('../utils/AppError');
+
+const sendErrorForDevelopmentEnv = (err, res) => {
+  const statusCode = err.statusCode || 500;
+  const status = err.status || 'error';
+  return res.status(statusCode).json({
+    status,
+    message: err.message,
+    err,
+    stack: err.stack
+  });
+};
+
+const sendErrorForProductionEnv = (err, res) => {
+  if (err.isOperationalError) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    return res.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error! Something Went Wrong'
+    });
+  }
+};
+
+const globalErrorHandler = (err, req, res, next) => {
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorForDevelopmentEnv(err, res);
+  }
+  else if (process.env.NODE_ENV === 'production') {
+    let error = Object.create(err);
+
+    /** Handle cast error */
+    if (error.name === 'CastError') {
+      const message = 'Invalid ID : ' + error.value;
+      error = new AppError(400, message);
+    }
+    /** Handle duplicate error */
+    if (error.code === 11000) {
+      const duplicateKeyValue = Object.entries(error.keyValue)[0];
+      const message = `Duplicate value for ${duplicateKeyValue[0]} : ${duplicateKeyValue[1]}. Already present in DB`;
+      error = new AppError(400, message);
+    }
+
+    /** Handle validation errors */
+    if (error.name === 'ValidationError') {
+      const message = Object.values(error.errors).map(current => current.message).join('. ');
+      error = new AppError(400, message);
+    }
+
+    sendErrorForProductionEnv(error, res);
+  }
+};
+
+module.exports = globalErrorHandler;
